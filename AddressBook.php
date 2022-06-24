6a8fd2f3f04ca7c017ab7df06e2efa285f1d17e2
mabbuprem <mabbupremkumar@gmail.com>
2022-06-24 07:04:40 +0530

    [prem] ADDED:Adding New Contacts In AddressBook

--- AddressBook.php
+++ AddressBook.php
@@ -1,23 +1,21 @@
 <?php
 
+
+include "ContactDetails.php";
+
 class AddressBookSystem
 {
-    public $firstName;
-    public $lasrName;
-    public $address;
-    public $city;
-    public $state;
-    public $zip;
-    public $phoneNumber;
-    public $email;
+
+    public $contactArray = [];
+    public $person;
+
 
     function welcome()
     {
         echo "Welcome to AddressBook system\n";
     }
-
-
-    function ContactDetails()
+    
+    function addContact()
     {
         $this->firstName  = readline("Enter your first name : ");
         $this->lastName   = readline("Enter your last name : ");
@@ -27,19 +25,27 @@ class AddressBookSystem
         $this->zip        = readline("Enter your zip code : ");
         $this->phoneNumber = readline("Enter your phone Number : ");
         $this->email      = readline("Enter your email : ");
+
+        $this->person = new ContactDetails($this->firstName, $this->lastName, $this->address, $this->city, $this->state, $this->zip, $this->phoneNumber, $this->email);
+        array_push($this->contactArray, $this->person);
+        $this->contactArray[] = $this->person;
+
+        $this->displayContactDetails();
     }
 
     function displayContactDetails()
     {
-        echo "Contact Details  : ";
-        echo "\nFirst Name     : " . $this->firstName;
-        echo "\nLast Name      : " . $this->lastName;
-        echo "\nAddress        : " . $this->address;
-        echo "\nCity           : " . $this->city;
-        echo "\nState          : " . $this->state;
-        echo "\nZip Code       : " . $this->zip;
-        echo "\nPhone Number   : " . $this->phoneNumber;
-        echo "\nEmail address  : " . $this->email;
+        for ($i = 0; $i < count($this->contactArray); $i++) {
+
+            echo "\nContactDetails :
+                 \nName : " . $this->person->getFirstName() . " " . $this->person->getLastName() . "\n"
+                . "Address : " . $this->person->getAddress() . "\n"
+                . "City : " . $this->person->getCity() . "\n"
+                . "State : " . $this->person->getState() . "\n"
+                . "ZipCode : " . $this->person->getZip() . "\n"
+                . "Phone Number : " . $this->person->getPhoneNumber() . "\n"
+                . "Email Id : " . $this->person->getEmail() . "\n";
+        }
     }
 }
 
@@ -47,5 +53,18 @@ class AddressBookSystem
 
 $addressBook = new AddressBookSystem();
 $addressBook->welcome();
-$addressBook->ContactDetails();
-$addressBook->displayContactDetails();
+while (true) {
+    $options = readline("\nEnter 1 to Add New Contact\nEnter 2 to Exit\n");
+    switch ($options) {
+        case 1:
+            $addressBook->addContact();
+            break;
+        case 2:
+            echo "Exit From AddressBook";
+            exit;
+
+            break;
+        case 3:
+            echo "Please Choose Option";
+    }
+}
